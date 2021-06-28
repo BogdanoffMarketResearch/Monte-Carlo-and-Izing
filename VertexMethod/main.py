ff104050db586f00d50c430b270c20ef5c2742f0
RomanEducation <58654270+RomanEducation@users.noreply.github.com>
2021-06-28 16:18:06 +0300

    update programme: tempature_index

--- VertexMethod/main.py
+++ VertexMethod/main.py
@@ -1,6 +1,5 @@
 import math
 import random as rn
-import Vertex as func
 from Vertex import (EquateLists, E, Sum_estimator_for_mean_E, coth)
 
 
@@ -12,8 +11,9 @@ dx = 0.5  # dx - сдвижка, меньше, чем указано в рабо
 N = 1  # N - число частиц
 D = 3  # D - размерность пространства
 b = [0.25, 0.5, 1, 2, 4]  # b - обратная температура
+temperature_index = 2  # Выбираем значение температуры из b
 E_max = 50  # Максимальная энергия
-N_mc = 10**6  # Число шагов Монте-Карло
+N_mc = 10**5  # Число шагов Монте-Карло
 X = []  # Задаем начальную конфигурацю: положим все значения координат равными нулю
 # Нам нужно две таблички для решения задачи, эта нужна, чтобы хранить где-то новую конфигурацию
 X_new_configuration = []
@@ -27,34 +27,34 @@ X_new_configuration.append(X_new_configuration[0])
 if __name__ == "__main__":
     Sum_estimators = 0
     for i in range(N_mc):
-        E_begin = E(X, 2)
+        E_begin = E(X, temperature_index)
         random_top = rn.randint(0, n - 1)  # выбираем случайную вершину
         for i in range(3):  # производим случайное изменение конфигурации
             Delta_x = (rn.random() - 0.5) * dx
             X_new_configuration[random_top][i] += Delta_x
         if random_top == 0:
             X_new_configuration[n] = X_new_configuration[random_top]
-        E_before = E(X_new_configuration, 2)
+        E_before = E(X_new_configuration, temperature_index)
         if E_before > E_max:
             EquateLists(X_new_configuration, X)
-            Sum_estimators += Sum_estimator_for_mean_E(X_new_configuration, 2)
+            Sum_estimators += Sum_estimator_for_mean_E(X_new_configuration, temperature_index)
             continue
         delta_E = E_before - E_begin
         if delta_E > 0:
             W = math.exp(-delta_E)  # вычисляем вероятность перехода
             if W >= rn.random():  # переходим или нет в новую конфигурацию
                 EquateLists(X, X_new_configuration)
-                Sum_estimators += Sum_estimator_for_mean_E(X, 2)
+                Sum_estimators += Sum_estimator_for_mean_E(X, temperature_index)
             else:
                 EquateLists(X_new_configuration, X)
-                Sum_estimators += Sum_estimator_for_mean_E(X, 2)
+                Sum_estimators += Sum_estimator_for_mean_E(X, temperature_index)
         else:
-            func.EquateLists(X, X_new_configuration)
-            Sum_estimators += Sum_estimator_for_mean_E(X, 2)
+            EquateLists(X, X_new_configuration)
+            Sum_estimators += Sum_estimator_for_mean_E(X, temperature_index)
 
     result = Sum_estimators / N_mc
 
-    Numerical_result = D / 2 * coth(b[2] / 2)
+    Numerical_result = D / 2 * coth(b[temperature_index] / 2)
 
     Error = (Numerical_result - result) / Numerical_result
     print('Результат Монте-Карло: %s' % result)
